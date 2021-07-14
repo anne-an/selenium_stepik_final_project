0052f54a0d8703a008b18153c44442f02c8f34f0
anne_morke <anne.morke@gmail.com>
2021-07-14 14:09:24 +0300

    Move login link methods to base page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import time
 
 import pytest
+
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
 
@@ -48,3 +50,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open(link)
     product_page.add_to_cart()
     product_page.should_disappear_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open(link)
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open(link)
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
